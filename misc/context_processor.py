1dd4be149d22718f7bca1c1e713cccbe33e61056
Denis Anikin <ad@xfenix.ru>
2014-05-18 18:46:40 +0400

    some important fixes

--- misc/context_processor.py
+++ misc/context_processor.py
@@ -1,4 +1,5 @@
 # vim:fileencoding=utf-8
+import json
 from hashlib import sha1
 from django.core.cache import cache
 from django.conf import settings
@@ -9,6 +10,16 @@ from misc.models import Menu, Partners, custom_settings
 
 # @cached(86400)
 def process(request):
+    def prepare_jquery():
+        try:
+            conf = json.loads(custom_settings.get('JQUERY_PATH', ''))
+            for item in conf:
+                if 'current' in item and item['current']:
+                    return item['path']
+        except ValueError:
+            pass
+        return ''
+
     """ default context processor
     add menu, partners and other wide site data
     """
@@ -20,4 +31,5 @@ def process(request):
         ismain=False if request.path.strip('/') else True,
         counter_yandex=custom_settings.get('COUNTER_YANDEX', ''),
         counter_google=custom_settings.get('COUNTER_GOOGLE', ''),
+        jquery_path=prepare_jquery(),
     )
