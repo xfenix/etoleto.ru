cbd56b262c516b7504cf534cfdebb5e3892eac81
Denis Anikin <ad@xfenix.ru>
2014-05-18 19:28:15 +0400

    fixes for better js

--- misc/tests.py
+++ misc/tests.py
@@ -14,23 +14,22 @@ class MiscTest(TestCase):
     def test_simple_flatpage(self):
         page = FlatPageFactory()
         self.assertTrue(
-            page.title in unicode(self.client.get(page.url))
+            page.title in self.client.get(page.url)
         )
 
     def test_failed_url_flatpage(self):
         self.assertTrue(
-            u'404' in unicode(self.client.get('/failed/').url)
+            u'404' in self.client.get('/failed/').url
         )
 
     def test_non_existing_template_flatpage(self):
         page = FlatPageFactory(template_name='non-existing-template.html')
         self.assertTrue(
-            page.title in unicode(self.client.get(page.url))
+            page.title in self.client.get(page.url)
         )
 
     def test_url_without_starting_flash_flatpage(self):
         page = FlatPageFactory()
-        print unicode(self.client.get(page.url[1:]))
         self.assertTrue(
-            page.title in unicode(self.client.get(page.url))
+            page.title in self.client.get(page.url[1:])
         )
